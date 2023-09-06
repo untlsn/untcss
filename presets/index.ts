1656ca24f785243fbf82d0316dc5a1b9733fc8c3
Filip Skoczeń <PhilipSkoczen@gmail.com>
2023-09-06 21:57:52 +0200

    Remove desktop-first as is now part of uno preset

--- presets/index.ts
+++ presets/index.ts
@@ -1,5 +1,4 @@
 export { default as presetAriaAndDataSelector } from './aria-and-data-selector';
-export { default as presetDesktopFirst } from './desktop-first';
 export { default as presetHocus } from './hocus';
 export { default as presetFlexibleGrid } from './flexible-grid';
 export { default as presetSizeShortcut } from './size-shortcut';
