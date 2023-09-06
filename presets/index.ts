1608070308d538f09fea62f13202b9120858b959
Filip Skoczeń <PhilipSkoczen@gmail.com>
2023-09-06 22:01:46 +0200

    Add my icons

--- presets/index.ts
+++ presets/index.ts
@@ -2,3 +2,4 @@ export { default as presetAriaAndDataSelector } from './aria-and-data-selector';
 export { default as presetHocus } from './hocus';
 export { default as presetFlexibleGrid } from './flexible-grid';
 export { default as presetSizeShortcut } from './size-shortcut';
+export { default as myPresetIcons } from './my-icons';
---
+++ presets/my-icons.ts
@@ -0,0 +1,42 @@
+import { Preset, presetIcons } from 'unocss';
+import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders';
+
+type MyIconsOptions = {
+	/**
+	 * Source of icons starting with my
+	 * @default './src/assets/icons'
+	 */
+	mySource?: string,
+	cdn?:      string,
+}
+
+// My preset for icons with some metadata
+export default function myPresetIcons(options: MyIconsOptions = {}): Preset {
+	return {
+		name:    'my-icons',
+		presets: [
+			presetIcons({
+				extraProperties: {
+					display:       'inline-block',
+					height:        'auto',
+					'min-height':  '1em',
+					'white-space': 'nowrap',
+				},
+				cdn:         options.cdn,
+				collections: {
+					// All icons placed inside src/assets/icons will be listed as i-my-${file-name}
+					my: FileSystemIconLoader(
+						options.mySource || './src/assets/icons',
+						(svg) => (
+							svg
+							// often black color is color of text, so can be replaced with currentColor
+								.replaceAll('#000', 'currentColor')
+								.replaceAll('#000000', 'currentColor')
+								.replaceAll('black', 'currentColor')
+						),
+					),
+				},
+			}),
+		],
+	};
+}
